eb00f7991b30e59bcef7090f4279d3cdc419fb8b
Raghav Naresh Kumar jha <39232349+RaghavJha1965@users.noreply.github.com>
2025-06-05 09:24:06 -0700

    chore : setting up the enviorment

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,30 @@
+"use client";
+// import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-black text-white py-4 px-6 flex items-center justify-between shadow-md">
+      {/* Logo */}
+      {/* <div className="flex items-center space-x-2">
+        <Image src="/logo.png" alt="Logo" width={50} height={50} />
+      </div> */}
+
+      {/* Links */}
+      <ul className="hidden md:flex items-center space-x-6 font-semibold uppercase tracking-wide text-sm">
+        <li><a href="#" className="hover:text-gray-400">About Us</a></li>
+        <li><a href="#" className="hover:text-gray-400">Registration</a></li>
+        <li><a href="#" className="hover:text-gray-400">Merchandise</a></li>
+        <li><a href="#" className="hover:text-gray-400">Media</a></li>
+        <li><a href="#" className="hover:text-gray-400">Proud Aurs</a></li>
+        <li>
+          <a
+            href="#"
+            className="border border-white rounded px-4 py-1 hover:bg-white hover:text-black transition"
+          >
+            Register Now
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <main className="flex items-center justify-center h-[80vh]">
+        <h1 className="text-4xl font-bold text-center">United S.T.O.R.M. Basketball</h1>
+      </main>
+    </div>
+  );
+}
